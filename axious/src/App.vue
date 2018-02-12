61988ebab7035aca940655d0153683083b6b1eb2
Reup67 <32143120+Reup67@users.noreply.github.com>
2018-02-12 14:41:20 +0300

    axious/

---
+++ axious/src/App.vue
@@ -0,0 +1,130 @@
+<template>
+    <div id="app">
+      <div class="head grid">
+        <div class="boxes1">
+          <p class="p1" @click="home"> Vue axious</p>
+        </div>
+        <div class="boxes2">
+          <p class="p2" @click="show">Sign up</p>
+        </div>
+        <div class="boxes3">
+          <p class="p3" @click="show2">Sign in</p>
+        </div>
+        <div class="boxes4">
+          <p class="p4">Dashboards</p>
+        </div>
+      </div>
+      <Test2 v-if="showSign"></Test2>
+      <Test v-if="showUp"></Test>
+     </div>
+</template>
+
+<script>
+import Test from './components/Test.vue';
+import Test2 from './components/Test2.vue';
+// import Test3 from './components/Test3.vue';
+// import Test4 from './components/Test4.vue';
+    export default {
+        data(){
+            return{
+              showSign:false,
+              showUp:false
+            };
+        },
+        components:{
+            Test,
+            Test2,
+            // Test3,
+            // Test4
+
+        },
+        methods:{
+            show(){
+              this.showSign = true;
+              this.showUp = false;
+            },
+            home(){
+              this.showSign = false;
+              this.showUp = false;
+            },
+            show2(){
+              this.showUp = true;
+              this.showSign = false;
+            }
+            // change(){
+            //     if ( this.showMe == false) {
+            //         this.showMe = true;
+            //     }else{
+            //         this.showMe = false;
+            //     }
+            // }
+        }
+    }
+</script>
+
+<style>
+
+   /* p{
+        position: absolute;
+    }
+    .div3{
+        width: 100%;
+        height: 90px;
+        background-color:   lightpink;
+        position: absolute;
+    }
+    .a1{
+        margin-left: 20%;
+        margin-top: 35px;
+        position: absolute;
+    }
+    .a2{
+        margin-left: 30%;
+        margin-top: 35px;
+        position: absolute;
+    }
+    .a3{
+        margin-left: 40%;
+        margin-top: 35px;
+        position: absolute;
+    }*/
+    .head{
+      width: 100%;
+      height: 80px;
+      background: #5d0555;
+    }
+    .grid{
+      display: grid;
+    }
+    .p1{
+      color: #fff;
+      font-size: 18px;
+      margin-left: 10%;
+      margin-top: 25px;
+      cursor: pointer;
+    }
+    .p2{
+      color: #de0d0d;
+      font-size: 18px;
+      margin-left: 65%;
+      margin-top: -38px;
+      position: absolute;
+      cursor: pointer;
+    }
+    .p3{
+      color: #fff;
+      font-size: 18px;
+      margin-left: 73%;
+      margin-top: -42px;
+      position: absolute;
+      cursor: pointer;
+    }
+    .p4{
+      color: #fff;
+      font-size: 18px;
+      margin-left: 88%;
+      margin-top: -46px;
+      position: absolute;
+      cursor: pointer;
+    }
+</style>
---
+++ axious/src/components/Test.vue
@@ -0,0 +1,47 @@
+<template>
+	<div class="in">
+		<p>Your email address {{email}}</p>
+	</div>
+</template>
+
+
+
+
+
+<script>
+	import axios from 'axios';
+    export default {
+			data(){
+				return{
+					email:''
+				}
+			},
+			created(){
+				axios.get('https://axios-23d4a.firebaseio.com/users.json')
+				.then(res =>{
+				console.log(res)
+				const data = res.data
+				const users = []
+				for(let key in data){
+					const user = data[key]
+					user.id = key
+					users.push(user)
+				}
+				console.log(users)
+				this.email = users[0].email
+				})
+				.catch(error => console.log(error))
+			}
+    }
+</script>
+
+
+
+<style>
+
+	.in{
+		width: 100%;
+		height: 800px;
+		background: lightpink;
+	}
+</style>
